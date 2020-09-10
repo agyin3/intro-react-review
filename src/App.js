be79f60f7d0aef7045464c535428fe5e3c674d99
Buddy Agyin <agyin3@gmail.com>
2020-09-10 16:49:15 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+// Make your axios call to https://api.openbrewerydb.org/breweries
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Brewery Fun</h1>
+    </div>
+  );
+}
+
+export default App;
