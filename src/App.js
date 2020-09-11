e6a4843e024666eed3b3cf6cead1d5c5d158b912
Buddy Agyin <agyin3@gmail.com>
2020-09-10 18:14:45 -0600

    consume api and render cards

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Axios from 'axios';
+import Breweries from './components/Breweries';
 
 // Make your axios call to https://api.openbrewerydb.org/breweries
 
 function App() {
+  const [state, setState] = useState([])
+
+  useEffect(() => {
+    Axios.get('https://api.openbrewerydb.org/breweries')
+      .then(res => {
+        setState(res.data)
+      })
+      .catch(err => console.log(err))
+  },[] )  
+
   return (
     <div className="App">
       <h1>Brewery Fun</h1>
+      <div className='main-wrapper'>
+        {state.map(brewery => <Breweries brewery={brewery} />)}
+      </div>
     </div>
   );
 }
--- src/components/Breweries.js
+++ src/components/Breweries.js
@@ -1,1 +1,16 @@
 // create your Brewery component here
+import React from 'react'
+
+const Breweries = (props) => {
+    console.log(props)
+    const { name, website_url, brewery_type } = props.brewery
+    return(
+        <div className='brewery'>
+            <h2>{name}</h2>
+            <p>Breeery Type: {brewery_type}</p>
+            <a href={website_url}>Visit Us Here</a>
+        </div>
+    )
+}
+
+export default Breweries
